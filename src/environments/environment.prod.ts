45a65da677b8f43ad86ef5aa61e9bdf48942dd4d
Vedant Patel <84228067+vedantpatel1997@users.noreply.github.com>
2023-05-18 02:27:59 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiAddress: 'https://localhost:7295/api/'
-  apiAddress: 'https://vkplibraryapi.azurewebsites.net/api/'
+  apiAddress: 'https://libraryprojectapi.azurewebsites.net/api/'
 
 };
